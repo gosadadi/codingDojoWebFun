858a86508db9ca727f5be6d340192658630a3166
gosa_dadi <gosa.d.dadi@gmail.com>
2022-08-20 13:22:14 -0700

    just practice

--- practice/practice.js
+++ practice/practice.js
@@ -1,7 +1,11 @@
 // function custom(element){
 //     console.log("This message is coming from Java Script",element);
 //     element.innerText="off";
-// }
-function hide(element) {
-    element.remove();
+function over(element) {
+    alert("mouseover");
 }
+
+function out(element) {
+    alert("mouseout");
+}
+
