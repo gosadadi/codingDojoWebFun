84f46bb23ae5d4e7f7fd7c9225dc19ea6a890356
gosa_dadi <gosa.d.dadi@gmail.com>
2022-08-22 11:45:30 -0700

    someupdate

--- JavaScript/dojoPizzeria.js
+++ JavaScript/dojoPizzeria.js
@@ -14,13 +14,22 @@
 // console.log(deliciousPizza3);
 // var deliciousPizza4 = pizzaOven("Classic Crust", "barbecue", ["Gorgonzola", "Gorgonzola"], ["Green Peppers", "Pineapple", "chicken", "tomatoes", "onion"]);
 // console.log(deliciousPizza4);
-function randomPizza(crustType, sauceType, cheese, toppings) {
-    let pizza = {};
-    pizza.crustType = crustType;
-    pizza.sauceType = sauceType;
-    pizza.cheese = cheese;
-    pizza.toppings = toppings;
-    return pizza;
-
-}
+var pizza={[
+    crustType: 'deep dish',
+    sauceType: 'traditional',
+    cheese: [ 'mozzarella', 'Cheddar' ],
+    toppings: [ 'pepperoni', 'sausage' ]];[
+    crustType: 'hand tossed',
+    sauceType: 'marinara',
+    cheese: [ 'mozzarella', 'feta' ],
+    toppings: [ 'mushrooms', 'olives', 'onions', 'tomatoes' ]];[
+    crustType: 'pan',
+    sauceType: 'buffalo',
+    cheese: [ 'Parmesan', 'Classic mozzarella ' ],
+    toppings: [ 'Mushrooms', 'Onions', 'Sausage', 'Black Olives', 'Pineapple' ]];[
+    crustType: 'Classic Crust',
+    sauceType: 'barbecue',
+    cheese: [ 'Gorgonzola', 'Gorgonzola' ],
+    toppings: [ 'Green Peppers', 'Pineapple', 'chicken', 'tomatoes', 'onion' ]]
+  }
 
--- buttonClicker/buttonClicker.js
+++ buttonClicker/buttonClicker.js
@@ -1,3 +1,6 @@
+function changeLog(element) {
+    element.innerText = "logout";
+}
 function removeButtonText(button){
 button.remove();
     console.log("I am working JS");
