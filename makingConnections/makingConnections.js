2038de82b3ae5bbaa0ccda3309fbbcb6cc2874cc
gosa_dadi <gosa.d.dadi@gmail.com>
2022-08-23 13:11:28 -0700

    updated ...

---
+++ makingConnections/makingConnections.js
@@ -0,0 +1,18 @@
+function changeUser() {
+    let x = document.querySelector("#Jane");
+    x.innerText = "John Doe";
+}
+function removeRequestTodd(element) {
+    element1 = document.querySelector(".ToddMain");
+    element1.remove();
+}
+function removeRequestPhil(element) {
+    element = document.querySelector(".PhilMain")
+    element.remove();
+}
+
+
+
+
+
+
