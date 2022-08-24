d4c8447861dfdf00b793a5f04e8e330cf0636a9a
gosa_dadi <gosa.d.dadi@gmail.com>
2022-08-24 14:37:38 -0700

    exam

--- likingLikes/partOne/likingLikesPartI.js
+++ likingLikes/partOne/likingLikesPartI.js
@@ -2,6 +2,5 @@ let count=3;
 function countElement(){
     let P3=document.querySelector("#liketracker");
     P3.innerText=count;
-    count++
-    return count;
+    count++;
 }
---
+++ yellowBeltExam/yellowBeltExam.js
@@ -0,0 +1,26 @@
+// function removeJoin(element){
+//     element=document.querySelector("#joinBtn");
+//     element.remove();
+// }
+// function clickHeart1(){
+// let x=document.querySelector("#increaseLikes1");
+// let count=x.innerText++;
+// return count;
+// }
+// function clickHeart2() {
+//     let x = document.querySelector("#increaseLikes2");
+//     let count=x.innerText++;
+//     return count;
+// }
+// function clickHeart3(){
+//     let x = document.querySelector("#increaseLikes3");
+//     let count=x.innerText++;
+//     return count;
+// }
+var nameTag=document.querySelector("#searchBtn");
+function setName(element){
+    nameTag.innerText=element.value;
+}
+
+
+
